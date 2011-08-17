0094daaac005a2a19119466cbc281a9732110d0a
GtoXic <rocky4126@hotmail.co.uk>
2011-08-18 00:30:21 +0100

    Added Multi-Channel logging.

--- bot.php
+++ bot.php
@@ -80,6 +80,9 @@ class IRCBot {
 		$host = $host[0];
 		$nick = substr($nick[0],1);
 		$chan = $this->ex[2];
+		$fp = fopen("./$chan.log", "a");
+		fwrite($fp, $data);
+		fclose($fp);
 		$command = str_replace(array(chr(10), chr(13)), "", $this->ex[3]);
 		$command = substr($command, 1);
 		include("./cmd/main.php");
