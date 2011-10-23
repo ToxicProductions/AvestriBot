bbe9f6ee7e37ff4698e73da7193384d257f919c8
GtoXic <rocky4126@hotmail.co.uk>
2011-10-23 03:50:45 +0200

    Update core/config.php

--- core/config.php
+++ core/config.php
@@ -5,7 +5,7 @@ $config = array(
 "channel" => "#avestri", //Channel of bot home and also for twitter tweets
 "name"    => "AvestriBot", //Bot's Realname
 "nick"    => "AvestriBot", //Bot's Nickname
-"pass"    => "m1er214", //Nickserv password (if applicable)
+"pass"    => "", //Nickserv password (if applicable)
 "owner"   => "GtoXic", //This is the bot owner or SuperUser which means they bypass the filters etc and do not need an admins.ini entry
 //MYSQL INFO
 "sqlhost" => "localhost", //MySQL Server IP/Hostname
